fdeb03424006c113fbf11c49db0941f47f81b10c
pgate1 <pgate.one@gmail.com>
2019-08-06 22:01:14 +0900

    mul_12 output width fix

--- mapper/fds/mul_12.v
+++ mapper/fds/mul_12.v
@@ -3,7 +3,7 @@ module mul_12 (
 	a, b, dout, con
 );
 	input [11:0] a, b; // unsigned
-	output [21:0] dout; // unsigned
+	output [23:0] dout; // unsigned
 	input con;
 
 	// -> •„†‚È‚µ 12 x 12 = 24 ƒrƒbƒgæŽZ
